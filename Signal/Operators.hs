cc8ebd699c46892e96eff2adaf8ac07436d47ee1
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-03 01:43:00 -0800

    Refactor signals to use real events

--- Signal/Operators.hs
+++ Signal/Operators.hs
@@ -9,9 +9,10 @@ module Signal.Operators ( filter
 
 import Data.IORef
 import Data.Monoid
-import Data.Maybe (isJust, isNothing)
+import Event
 import Prelude hiding (filter, take)
 import Signal
+import Subscriber
 
 -- | Filters the values of a signal according to a predicate.
 filter :: Signal a -> (a -> Bool) -> Signal a
@@ -19,25 +20,25 @@ filter s f =
     let f' x = if f x then return x else mempty
     in s >>= f'
 
--- | Runs the function whenever signal sends an event.
-doEvent :: Signal a -> (Maybe a -> IO ()) -> Signal a
+-- | Runs a function whenever the signal sends an event.
+doEvent :: Signal a -> (Event a -> IO ()) -> Signal a
 doEvent s f =
     signal $ \sub ->
-        let onEvent m = f m >> sub m
+        let onEvent e = f e >> send sub e
         in s >>: onEvent
 
 -- | Runs a function on each value.
 doNext :: Signal a -> (a -> IO ()) -> Signal a
 doNext s f =
-    let f' (Just x) = f x
-        f' Nothing = return ()
+    let f' (NextEvent x) = f x
+        f' _ = return ()
     in doEvent s f'
 
--- | Runs the function on each completion.
+-- | Runs a function on completion.
 doCompleted :: Signal a -> IO () -> Signal a
 doCompleted s f =
-    let f' (Just x) = return ()
-        f' Nothing = f
+    let f' CompletedEvent = f
+        f' _ = return ()
     in doEvent s f'
 
 -- | Returns a signal of the first @n@ elements.
@@ -46,19 +47,19 @@ take s n =
     signal $ \sub -> do
         remRef <- newIORef n
 
-        let onNext Nothing = do
-                b <- atomicModifyIORef remRef $ \rem ->
-                    (0, if rem == 0 then False else True)
-
-                if b then sub Nothing else return ()
-
-            onNext (Just v) = do
+        let onEvent ev@(NextEvent _) = do
                 old <- atomicModifyIORef remRef $ \rem ->
                     if rem == 0 then (0, 0) else (rem - 1, rem)
 
                 case old of
                     0 -> return ()
-                    1 -> sub (Just v) >> sub Nothing
-                    _ -> sub (Just v)
+                    1 -> send sub ev >> send sub CompletedEvent
+                    _ -> send sub ev
 
-        s >>: onNext
+            onEvent ev = do
+                b <- atomicModifyIORef remRef $ \rem ->
+                    (0, if rem == 0 then False else True)
+
+                if b then send sub ev else return ()
+
+        s >>: onEvent
