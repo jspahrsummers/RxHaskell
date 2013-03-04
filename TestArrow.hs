395ab59f97845460377af5e36e72dbc670e3092d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-03 22:37:40 -0800

    Add a simple Arrow instance for Signal transformers?

---
+++ TestArrow.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Arrows #-}
+
+module TestArrow where
+
+import Control.Arrow
+import Signal
+import Signal.Arrow
+import Signal.Operators
+
+testConcat = do
+    let a :: SignalArrow String String
+        a =
+            proc x -> do
+                foo <- arr (++ "foo") -< x
+                bar <- arr (++ "bar") -< x
+                returnA -< (foo ++ ":" ++ bar)
+
+    (runSignalArrow a) (fromFoldable ["hello", "world"])
+        >>: print
