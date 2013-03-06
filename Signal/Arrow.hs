21c170807986cebe93d31786f5fe6598875ad2ab
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-05 22:04:55 -0800

    Add an ArrowChoice instance

--- Signal/Arrow.hs
+++ Signal/Arrow.hs
@@ -7,12 +7,17 @@ module Signal.Arrow ( SignalArrow
 import Control.Arrow
 import Control.Category
 import Control.Monad
+import Event
 import Prelude hiding ((.), id)
 import Signal
 
 newtype SignalArrow b c = SignalArrow {
     runSignalArrow :: Signal b -> Signal c
 }
+
+instance Category SignalArrow where
+    (SignalArrow g) . (SignalArrow f) = SignalArrow (g . f)
+    id = arr id
 
 instance Arrow SignalArrow where
     arr = SignalArrow . liftM
@@ -23,6 +28,11 @@ instance Arrow SignalArrow where
                     in fmap (flip (,) d) cs
         in SignalArrow first'
 
-instance Category SignalArrow where
-    (SignalArrow g) . (SignalArrow f) = SignalArrow (g . f)
-    id = arr id
+instance ArrowChoice SignalArrow where
+    left (SignalArrow a) =
+        let onNext (Left b) =
+                let cs = a $ return b
+                in fmap Left cs
+
+            onNext (Right d) = return $ Right d
+        in SignalArrow (>>= onNext)
