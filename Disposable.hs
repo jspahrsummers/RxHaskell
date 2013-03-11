e5885b32e7b3037deb1a8740eb4de70f8b500b5e
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-10 17:26:29 -0700

    Refactor disposables like cray

--- Disposable.hs
+++ Disposable.hs
@@ -1,57 +1,79 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE Safe #-}
 
-module Disposable ( Disposable
-                  , dispose
+module Disposable ( Disposable(EmptyDisposable)
                   , newDisposable
-                  , newCompositeDisposable
-                  , empty
-                  , addDisposable
+                  , DisposableSet
+                  , newDisposableSet
+                  , toDisposable
                   ) where
 
-import Control.Applicative ((<$), (<$>))
+import Control.Applicative
 import Control.Monad
+import Data.Foldable as F
+import Data.Sequence as Seq
 import Data.IORef
-
--- | A list of disposables, along with a flag indicating whether disposal already happened.
-type DisposableList = (Bool, [Disposable])
+import Data.Unique
 
 -- | Represents an operation which can be canceled or a resource which can be freed.
-data Disposable = ActionDisposable (IO ()) (IORef Bool)
-                | CompositeDisposable (IORef DisposableList)
-                | EmptyDisposable
+data Disposable = EmptyDisposable
+                | Disposable Unique (IORef MaybeAction)
+
+instance Eq Disposable where
+    EmptyDisposable == EmptyDisposable = True
+    (Disposable u _) == (Disposable u' _) = u == u'
+    _ == _ = False
+
+-- | @Just f@ when not yet disposed. @Nothing@ after disposal.
+type MaybeAction = Maybe (IO ())
 
 -- | Disposes a disposable.
 dispose :: Disposable -> IO ()
 dispose EmptyDisposable = return ()
-dispose (ActionDisposable action d) = do
-    b <- atomicModifyIORef d $ \b -> (True, b)
-    unless b action
-
-dispose (CompositeDisposable dl) = 
-    let disposedList = (True, [])
-        modify (True, _) = (disposedList, disposedList)
-        modify (False, xs) = (disposedList, (False, xs))
-    in do
-        (b, xs) <- atomicModifyIORef dl modify
-        unless b $ mapM_ dispose xs
+dispose (Disposable _ mref) = do
+    m <- atomicModifyIORef mref $ \m -> (Nothing, m)
+    maybe (return ()) id m
 
 -- | Creates a disposable which runs the given action upon disposal.
 newDisposable :: IO () -> IO Disposable
-newDisposable action = ActionDisposable action <$> newIORef False
+newDisposable action = liftM2 Disposable newUnique $ newIORef $ Just action
 
--- | Creates a disposable which can dispose of other disposables.
-newCompositeDisposable :: IO Disposable
-newCompositeDisposable = CompositeDisposable <$> newIORef (False, [])
+-- | @Just s@ when not yet disposed. @Nothing@ after disposal.
+type MaybeSet = Maybe (Seq Disposable)
 
--- | Adds disposable @d@ to the composite disposable.
-addDisposable :: Disposable -> Disposable -> IO ()
-addDisposable (CompositeDisposable dl) d =
-    let modify (True, _) = ((True, []), True)
-        modify (False, xs) = ((False, d : xs), False)
+-- | A synchronized set of disposables.
+newtype DisposableSet = DisposableSet (IORef MaybeSet)
+
+-- | Creates a set of disposables.
+newDisposableSet :: IO DisposableSet
+newDisposableSet = DisposableSet <$> newIORef (Just Seq.empty)
+
+-- | Converts a set of disposables into a disposable.
+-- | The constructed disposable will dispose of all disposables in the set.
+toDisposable :: DisposableSet -> IO Disposable
+toDisposable (DisposableSet mref) =
+    let disposeSet :: Seq Disposable -> IO ()
+        disposeSet = F.mapM_ dispose
+
+        action :: IO ()
+        action = do
+            m <- atomicModifyIORef mref $ \m -> (Nothing, m)
+            maybe (return ()) disposeSet m
+    in newDisposable action
+
+-- | Adds a disposable to a set.
+addDisposable :: DisposableSet -> Disposable -> IO ()
+addDisposable (DisposableSet mref) d =
+    let addDisposable' :: MaybeSet -> (MaybeSet, Bool)
+        addDisposable' Nothing = (Nothing, True)
+        addDisposable' (Just s) = (Just $ s |> d, False)
     in do
-        b <- atomicModifyIORef dl modify
+        b <- atomicModifyIORef mref addDisposable'
         when b $ dispose d
 
--- | Returns a disposable which does no work.
-empty :: Disposable
-empty = EmptyDisposable
+-- | Removes a disposable from a set.
+removeDisposable :: DisposableSet -> Disposable -> IO ()
+removeDisposable (DisposableSet mref) d =
+    let removeDisposable' :: MaybeSet -> MaybeSet
+        removeDisposable' = liftM $ Seq.filter (/= d)
+    in atomicModifyIORef mref $ \m -> (removeDisposable' m, ())
