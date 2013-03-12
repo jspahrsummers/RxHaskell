afdf5ab6cf36c012a0fe3db36cda2373a6e8aca2
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-03-12 00:47:47 -0700

    Keep scheduler threads alive only as long as necessary

--- Scheduler.hs
+++ Scheduler.hs
@@ -5,50 +5,52 @@ module Scheduler ( Scheduler
                  , schedule
                  ) where
 
+import Control.Applicative
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
 import Data.IORef
+import Data.Maybe
 import Disposable
 
 -- | Represents an action on a scheduler, along with a flag indicating whether it should be canceled.
 type ScheduledAction = (IORef Bool, IO ())
 
 -- | Executes actions serially in FIFO order.
-data Scheduler = Scheduler {
-    thread :: ThreadId,
-    queue :: TQueue ScheduledAction
-}
+newtype Scheduler = Scheduler (TQueue ScheduledAction)
 
 -- | Creates a new background scheduler.
 newScheduler :: IO Scheduler
-newScheduler = do
-    q <- atomically newTQueue
-
-    -- TODO: Spawn scheduler threads lazily (when the first action is enqueued).
-    tid <- forkIO $ schedulerMain q
-
-    return Scheduler {
-        thread = tid,
-        queue = q
-    }
+newScheduler = Scheduler <$> atomically newTQueue
 
 -- | Schedules @action@ on @s@. Returns a disposable which can be used to cancel it.
 schedule :: Scheduler -> IO () -> IO (Disposable IO)
-schedule s action = do
+schedule s@(Scheduler q) action = do
     ref <- newIORef False
-    atomically $ writeTQueue (queue s) (ref, action)
+
+    let schedule' = do
+            e <- isEmptyTQueue q
+            writeTQueue q (ref, action)
+            return e
+
+    e <- atomically schedule'
+
+    -- If the queue was previously empty, spin up a thread for the scheduler.
+    when e $ void $ forkIO $ schedulerMain s
 
     newDisposable $ atomicModifyIORef ref $ const (True, ())
 
 -- | Executes all current and future actions enqueued on the given scheduler.
-schedulerMain :: TQueue ScheduledAction -> IO ()
-schedulerMain q = do
-    -- TODO: If the queue is empty, we should kill this scheduler's thread until a new action is enqueued.
-    (ref, action) <- atomically $ readTQueue q
+schedulerMain :: Scheduler -> IO ()
+schedulerMain s@(Scheduler q) =
+    let schedulerMain' (ref, action) = do
+            d <- readIORef ref
+            unless d action
 
-    d <- readIORef ref
-    unless d action
+            yield
+            schedulerMain s
+    in do
+        m <- atomically $ tryReadTQueue q
 
-    yield
-    schedulerMain q
+        -- If the queue is empty, let this thread die.
+        maybe (return ()) schedulerMain' m
